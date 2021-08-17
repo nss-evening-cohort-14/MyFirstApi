846a4fb432db8fba809a42ab3de6bb619384276b
Nathan Gonzalez <ncgonz@gmail.com>
2021-08-16 21:13:47 -0500

    Add project files.

---
+++ MyFirstApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstApi.Controllers
+{
+    [ApiController]
+    [Route("WeatherForecast")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        //     /weatherforecast/numbers
+        [HttpGet("numbers")]
+        public List<int> GetNumbers()
+        {
+            return new List<int> {1,3,5,7,9};
+        }
+
+        // /weatherforecast
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
